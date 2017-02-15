b5c977cbfb8ff8c1754911bc987aec7062559324
Bao Trinh <qubidt@Baos-Mac.local>
2017-02-15 09:00:48 -0800

    Initial Commit

---
+++ MemoryCap/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemoryCap
+//
+//  Created by Bao Trinh on 2/15/17.
+//  Copyright © 2017 Bao Trinh. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
