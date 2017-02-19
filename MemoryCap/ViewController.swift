418923c6d9dc7e2ecdc176781164d61a3f202833
Bao Trinh <qubidt@baos-mac.local>
2017-02-19 07:32:43 -0600

    Implemented display and zoom in on user's location

--- MemoryCap/ViewController.swift
+++ MemoryCap/ViewController.swift
@@ -7,12 +7,69 @@
 //
 
 import UIKit
+import CoreLocation
+import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, CLLocationManagerDelegate {
 
+    var locationManager : CLLocationManager!
+    
+    @IBOutlet weak var mapView: MKMapView!
+    
+    // MARK: Setup
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        mapView.showsUserLocation = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        determineMyCurrentLocation()
+    }
+    
+    // MARK: Location Management
+
+    func determineMyCurrentLocation() {
+        locationManager = CLLocationManager()
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.requestAlwaysAuthorization()
+        
+        if CLLocationManager.locationServicesEnabled() {
+            locationManager.startUpdatingLocation()
+            //locationManager.startUpdatingHeading()
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        let userLocation:CLLocation = locations[0] as CLLocation
+        
+        // Call stopUpdatingLocation() to stop listening for location updates,
+        // other wise this function will be called every time when user location changes.
+        
+        // manager.stopUpdatingLocation()
+        
+        
+        // print("user latitude = \(userLocation.coordinate.latitude)")
+        // print("user longitude = \(userLocation.coordinate.longitude)")
+        
+        let latitude:CLLocationDegrees = userLocation.coordinate.latitude
+        let longitude:CLLocationDegrees = userLocation.coordinate.longitude
+        let latDelta:CLLocationDegrees = 0.02
+        let lonDelta:CLLocationDegrees = 0.02
+        let span = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location = CLLocationCoordinate2DMake(latitude, longitude)
+        let region = MKCoordinateRegionMake(location, span)
+        mapView.setRegion(region, animated: true)
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error)
+    {
+        print("Error \(error)")
     }
 
     override func didReceiveMemoryWarning() {
